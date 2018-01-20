9caf1e72c06143ee79b181861451cac994e037f5
Jasper Spruytte <jaspergj.spruytte@gmail.com>
2018-01-20 09:13:20 +0100

    User can answer question

--- src/main/java/be/jasperspruytte/sillyquiz/SillyQuiz.java
+++ src/main/java/be/jasperspruytte/sillyquiz/SillyQuiz.java
@@ -1,4 +1,20 @@
 package be.jasperspruytte.sillyquiz;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class SillyQuiz {
+
+    private static BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String args[]) throws IOException {
+        System.out.println("What's up, dog?");
+        String response = userInput.readLine();
+        if (response == "Nothing much") {
+            System.out.println("Cool, dawg.");
+        } else {
+            System.out.println("'" + response + "'? Really? I mean, really?");
+        }
+    }
 }
